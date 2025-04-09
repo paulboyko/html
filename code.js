dae39c6b874bed63b52fc3c6a8960ffa366fffc8
paulboyko <51275346+paulboyko@users.noreply.github.com>
2025-04-09 11:55:21 -0400

    Update code.js

--- code.js
+++ code.js
@@ -1,1 +1,7 @@
+  const boykoDiv = document.getElementById('boyko');
+  const infoDiv = document.getElementById('info');
 
+  // Add a click event listener to the 'boyko' div
+  boykoDiv.addEventListener('click', function() {
+      infoDiv.textContent = 'This is Mr. Boyko\'s info';
+  });
